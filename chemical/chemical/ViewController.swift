69beb46ed2ae29400c484825c90cbf6a60b85aeb
Tunscopi <Tunscopi@News-MacBook-Pro.local>
2016-09-17 09:33:19 -0400

    Initial Table view, not populating though

---
+++ chemical/chemical/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  chemical
+//
+//  Created by Tunscopi on 9/16/16.
+//  Copyright © 2016 Tunscopi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    let chemicalHack = ["Ayo", "Roshan", "Ram", "Ashim"];
+    let textCellIdentifier = "TextCell"
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chemicalHack.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath)
+        
+        let row = indexPath.row
+        cell.textLabel?.text = chemicalHack[row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let row = indexPath.row
+        print (chemicalHack[row])
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
